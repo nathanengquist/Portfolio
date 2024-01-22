6c3fc6cb552a09e66449e2b66689707aeaeb65b7
Nathan Engquist <nathanengquist@users.noreply.github.com>
2024-01-22 16:34:18 -0700

    Update next.config.js
    removing image optimization on the server side since not working

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // ... other config options
+  images: {
+    unoptimized: true, // Disable Next.js's built-in image optimization
+  },
   // Removed the `output: 'export'` line
 }
 
